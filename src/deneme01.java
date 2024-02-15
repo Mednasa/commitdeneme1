a4396f25b1c9cbfae366bb71439297e33cfaada8
Mednasa <girgin183@gmail.com>
2024-02-15 12:20:15 +0300

    son güncelleme

--- src/deneme01.java
+++ src/deneme01.java
@@ -2,5 +2,6 @@ public class deneme01 {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println(" Hello World");
+        System.out.println("HELLO WORLD");
     }
 }
